4c4841df0330c2618470bde658c540210cbd8645
Lara <larabernspereira@gmail.com>
2024-02-20 14:49:26 -0300

    🔧 chore: Preparando o ambiente com requisicoes GET, DELETE e POST e conexao com MongoDB

---
+++ index.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var cors = require('cors');
+
+require('dotenv').config();
+
+var app = express();
+app.use(express.json());
+app.use(cors());
+
+var CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+var DATABASENAME = 'kanban'
+var database
+
+app.listen(3002, () => {
+    MongoClient.connect(CONNECTION_STRING)
+        .then((client) => {
+            database = client.db(DATABASENAME);
+            console.log("CONECTADO COM SUCESSO (:");
+        })
+        .catch((error) => {
+            console.error("Error connecting to MongoDB:", error);
+        });
+})
+
+
+// Get all tasks
+app.get('/api/kanban/getalltasks', (request, response) => {
+    database.collection('kanban-tasks').find({}).toArray((error, result) => {
+        response.send(result)
+    })
+})
+
+// Post task
+app.post('/api/kanban/posttask', express.json(), (request, response) => {
+    database.collection('kanban-tasks').count({}, function (error, numOfDocs) {
+        const newTask = {
+            id: (numOfDocs + 1).toString(),
+            title: request.body.title,
+            completed: request.body.completed,
+            doing: request.body.doing,
+            completionDate: request.body.completionDate
+        };
+
+        database.collection('kanban-tasks').insertOne(newTask, (error, result) => {
+            if (error) {
+                console.error("Error adding new task:", error);
+                response.status(500).json("Error adding new task");
+            } else {
+                response.json('Adicionada nova task');
+            }
+        });
+    });
+});
+
+
+// Delete task
+app.delete('/api/kanban/deletetask', async (request, response) => {
+    try {
+        const result = await database.collection('kanban-tasks').deleteOne({
+            id: request.query.id
+        });
+
+        if (result.deletedCount === 1) {
+            response.json("Task deleted successfully");
+        } else {
+            response.status(404).json("Task not found");
+        }
+    } catch (error) {
+        console.error("Error deleting task:", error);
+        response.status(500).json("Error deleting task");
+    }
+});
---
+++ readme.md
@@ -0,0 +1,38 @@
+# Desafio Técnico: Aplicativo de Kanban com ReactJS e NodeJS
+
+Este repositório contém o código-fonte para um aplicativo simples de Kanban, desenvolvido com ReactJS para o frontend e Node.js para o backend. O objetivo é permitir que os usuários gerenciem tarefas através das três colunas: "To Do", "Doing" e "Ready".
+
+## Backend (Node.js com Express)
+
+### Glossário
+
+- 🔲 Atividade a ser elaborada.
+- ✅ Atividade feita.
+- ⚠️ Atividade feita com observações.
+- ❌ Atividade não realizada.
+
+### Requisitos:
+
+- 🔲 Desenvolver uma API para criar, atualizar, mover e deletar tarefas.
+- 🔲 A API deve permitir a filtragem e ordenação de tarefas.
+- ❌ Implementar autenticação básica (opcional, para candidatos visando posições mais avançadas).
+
+### Banco de Dados:
+
+- 🔲 Utilizar uma das opções para armazenamento de dados: SQLite, Postgres, Mysql ou Mongodb.
+- 🔲 Projetar um esquema de banco de dados simples para gerenciar as tarefas e seu estado.
+
+### Práticas de Desenvolvimento:
+
+- 🔲 Escrever código limpo e bem organizado, seguindo padrões de código.
+- 🔲 Implementar tratamento de erros e logging adequados.
+
+### Como Executar o Backend:
+
+1. Clone este repositório: `git clone https://github.com/laraberns/kanban-backend.git`
+2. Instale as dependências: `npm install`
+3. Inicie o servidor: `npm start`
+
+A API estará disponível em [http://localhost:3002](http://localhost:3002) para interação com o frontend.
+
+Sinta-se à vontade para explorar, contribuir e aprimorar este projeto!
